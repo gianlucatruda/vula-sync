22646796e64546d87185a187f2e078cc000bada8
Gianluca Truda <gianlucatruda@gmail.com>
2018-02-20 10:44:06 +0200

    Updated locations for 2018 courses and experimental code for unmounting drives after use.

--- sync.sh
+++ sync.sh
@@ -1,14 +1,12 @@
 #!/usr/local/bin/bash
 # Vula course resource synchroniser
-# Version 5.0
+# Version 6.0
 # Gianluca Truda
-# 14 August 2017
+# 20 February 2018
 
 # Fill in the details below
 
-MAM="48efc4a8-7cd8-4897-a873-227f1681d91e"
-CSC="36d78fc9-75b1-4f6c-a810-e71ed865d415"
-MCB="cf74494c-7c8c-4bfe-bd61-87b04a8ed405"
+CSC="8139d0af-88f1-492c-9ee6-58f1655428e8"
 DIR="/Users/gianlucatruda/Documents/GIANLUCA/Current/resources"
 
 #-------------------------------
@@ -22,20 +20,16 @@ function SYNC {
 	echo
 	echo $DATE
 	echo
-	echo Mounting Drives…
-	osascript -e ' mount volume "https://vula.uct.ac.za/dav/'$MAM'" '
+	echo Clearing Volumes...
+	rmdir //Volumes/*
+	echo
+	echo Mounting Drives...
 	osascript -e ' mount volume "https://vula.uct.ac.za/dav/'$CSC'" '
-	osascript -e ' mount volume "https://vula.uct.ac.za/dav/'$MCB'" '
 	cd /Users/gianlucatruda/Desktop
 	echo Synchronising…
-	rsync -avzh --progress //Volumes/$MAM/* $DIR/mam
-	rsync -avzh --progress //Volumes/$CSC/* $DIR/csc
-	rsync -avzh --progress //Volumes/$MCB/* $DIR/mcb
-	echo Unmounting Drives…
-	umount /Volumes/$MAM
-	umount /Volumes/$CSC
-	umount /Volumes/$MCB
-
+	rsync -avzh --progress //Volumes/$CSC'-1'/* $DIR/csc
+	# echo Unmounting Drives…
+	# diskutil unmount //Volumes/$CSC'-1'
 	echo Sync Complete on $DATE
 	echo
 	echo _______________________________
